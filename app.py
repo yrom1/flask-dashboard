d478d2b4b56cf81ccd29cad7bc09e2c0238633da
yrom1 <ryanm.inbox@gmail.com>
2022-09-10 03:49:58 -0400

    Add template example, this looks good

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 # save this as app.py
 from flask import Flask
+from markupsafe import escape
+from flask import url_for
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template("hello.html", name=name)
