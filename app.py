c45f12a3c385c4bdf6312d0e10a2269bc22c59a4
yrom1 <ryanm.inbox@gmail.com>
2022-09-10 05:28:02 -0400

    Basic multi page rendering HTMl

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from markupsafe import escape
 from flask import url_for
 from flask import render_template
+from markupsafe import Markup
 
 app = Flask(__name__)
 
@@ -14,5 +15,10 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/<page>")
 def hello(page=None):
-    return render_template("layout.html", page=page)
+    if page == "test":
+        content = Markup("<h1>TEST TEST TEST</h1>")
+    else:
+        content = "Hi!"
+    return render_template("layout.html", content=content)
