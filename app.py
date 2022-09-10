1060a00ab52293466f84e73ffc041488b48639ce
yrom1 <ryanm.inbox@gmail.com>
2022-09-10 04:57:04 -0400

    Completely broken main page, but it's a start

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello(name=None):
-    return render_template("hello.html", name=name)
+# @app.route("/hello/")
+# @app.route("/hello/<name>")
+# def hello(name=None):
+#     return render_template("hello.html", name=name)
+
+
+@app.route("/")
+def hello(page=None):
+    return render_template("layout.html", page=page)
